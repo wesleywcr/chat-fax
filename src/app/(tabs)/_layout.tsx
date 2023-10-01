90c663992f43454ef430378c06bdc8b3fdc9e369
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-09-30 23:02:24 -0300

    feat: :sparkles: add navigation tabs

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,49 @@
+import {
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
+  useFonts,
+} from '@expo-google-fonts/poppins';
+import { SplashScreen, Tabs } from 'expo-router';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function TabRoutesLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+  });
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      // Hide the splash screen after the fonts have loaded (or an error was returned) and the UI is ready.
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  // Prevent rendering until the font has loaded or an error was returned
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'home',
+        }}
+      />
+    </Tabs>
+  );
+}
