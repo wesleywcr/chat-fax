feb6d25df4de960a3eed073d45faab792198f8cf
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-09-30 23:03:09 -0300

    feat: :sparkles: add expo router

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './App.{js,jsx,ts,tsx}',
+    './src/app/**/*.{js,jsx,ts,tsx}',
     './src/components/**/*.{js,jsx,ts,tsx}',
     './src/screens/**/*.{js,jsx,ts,tsx}',
   ],
