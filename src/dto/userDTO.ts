67fd767370aa92a8c63c087706da6101eb93acb1
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-10-31 23:22:09 -0300

    feat: add new type user DTO

---
+++ src/dto/userDTO.ts
@@ -0,0 +1,13 @@
+export type UserDTO = {
+  id: string;
+  collectionId: string;
+  collectionName: string;
+  username?: string;
+  verified?: boolean;
+  emailVisibility?: boolean;
+  email?: string;
+  created?: string;
+  updated?: string;
+  name?: string;
+  avatar?: string;
+};
