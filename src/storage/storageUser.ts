7cfde36399fdf3dd805da1252aaecb7b9f0ed682
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-10-31 23:21:52 -0300

    feat: add storages in app

---
+++ src/storage/storageUser.ts
@@ -0,0 +1,20 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import type { UserDTO } from 'dto/userDTO';
+
+import { USER_STORAGE } from './storageVariables';
+
+export async function storageUserSave(user: UserDTO) {
+  await AsyncStorage.setItem(USER_STORAGE, JSON.stringify(user));
+}
+
+export async function storageUserGet() {
+  const storage = await AsyncStorage.getItem(USER_STORAGE);
+
+  const user: UserDTO = storage ? JSON.parse(storage) : {};
+
+  return user;
+}
+
+export async function storageUserRemove() {
+  await AsyncStorage.removeItem(USER_STORAGE);
+}
---
+++ src/storage/storageVariables.ts
@@ -0,0 +1,2 @@
+export const USER_STORAGE = '@chatfax:user';
+export const AUTH_TOKEN_STORAGE = '@chatfax:token';
