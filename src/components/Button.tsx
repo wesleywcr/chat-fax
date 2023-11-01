edf1617eb3e63b7735b2c395c2597e4c874f1ef4
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-10-31 23:22:41 -0300

    feat: :sparkles: add component button

---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import type { TouchableOpacityProps } from 'react-native';
+import { ActivityIndicator, Text, TouchableOpacity, View } from 'react-native';
+
+type ButtonProps = TouchableOpacityProps & {
+  title: string;
+  isLoading?: boolean;
+};
+export function Button({ title, isLoading, ...rest }: ButtonProps) {
+  return (
+    <View className="w-full">
+      <TouchableOpacity
+        className="h-14 w-full items-center
+      justify-center rounded-md bg-blue-600"
+        {...rest}
+      >
+        {isLoading ? (
+          <ActivityIndicator color="#000" />
+        ) : (
+          <Text className="font-bold text-white">{title}</Text>
+        )}
+      </TouchableOpacity>
+    </View>
+  );
+}
