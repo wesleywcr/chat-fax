a6fdbe0ffcbc43cfe38243e1376115ba30bc5921
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-11-02 22:37:26 -0300

    feat: add signUp schema

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-export const signUpSchema = z.object({
+export const signInSchema = z.object({
   email: z
     .string({ required_error: 'E-mail é um campo obrigatório' })
     .email({ message: 'O campo deve conter um e-mail válido' }),
@@ -8,3 +8,20 @@ export const signUpSchema = z.object({
     .string({ required_error: 'Senha é um campo obrigatório' })
     .min(6, { message: 'A senha deve ter no mínimo 6 caracteres' }),
 });
+export const signUpSchema = z
+  .object({
+    name: z.string({ required_error: 'Nome é um campo obrigatório' }),
+    email: z
+      .string({ required_error: 'E-mail é um campo obrigatório' })
+      .email({ message: 'O campo deve conter um e-mail válido' }),
+    password: z
+      .string({ required_error: 'Senha é um campo obrigatório' })
+      .min(8, { message: 'A senha deve ter no mínimo 8 caracteres' }),
+    passwordConfirm: z
+      .string({ required_error: 'Confirmação de senha é um campo obrigatório' })
+      .min(8, { message: 'A senha deve ter no mínimo 8 caracteres' }),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    path: ['confirmNewPassword'],
+    message: 'As senhas não correspondem',
+  });
