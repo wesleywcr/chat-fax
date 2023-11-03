61f70d5838a9248ed77ed49d32e884d57dda6f2a
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-11-02 22:39:15 -0300

    fix: :bug: add eventsource

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,3 +1,6 @@
 import PocketBase from 'pocketbase';
+import eventsource from 'react-native-sse';
+// @ts-ignore
+global.EventSource = eventsource;
 
 export const pb = new PocketBase('http://10.0.2.2:8090');
