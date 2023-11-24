8bc981a789fd7c199bf9a8e398d08d684d0520bf
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-11-23 23:15:09 -0300

    refactor: :arrow_down: downgrade expo version [SDK:48.0.21]

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,34 @@
-// eslint-disable-next-line func-names
 module.exports = function (api) {
   api.cache(true);
+  // Add the line below (this path is relative from `node_modules/expo-router`)
+  process.env.EXPO_ROUTER_APP_ROOT = '../../src/app';
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel', 'expo-router/babel'],
+    plugins: [
+      'nativewind/babel',
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-reanimated/plugin',
+      require.resolve('expo-router/babel'),
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@dto': './src/dto',
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@storage': './src/storage',
+            '@utils': './src/utils',
+            '@services': './src/services',
+            '@hooks': './src/hooks',
+            '@contexts': './src/contexts',
+            '@lib': './src/lib',
+            '@schemas': './src/schemas',
+          },
+        },
+      ],
+    ],
   };
 };
