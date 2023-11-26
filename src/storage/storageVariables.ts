bb3bfe6ffda67f98e23a690576642857e2535473
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-11-25 23:28:42 -0300

    feat: add variable FILE_URL and new functions convert date

--- src/storage/storageVariables.ts
+++ src/storage/storageVariables.ts
@@ -1,2 +1,3 @@
 export const USER_STORAGE = '@chatfax:user';
 export const AUTH_TOKEN_STORAGE = '@chatfax:token';
+export const FILE_URL = 'http://10.0.2.2:8090/api/files/_pb_users_auth_';
---
+++ src/utils/convertTime.ts
@@ -0,0 +1,37 @@
+export function convertDateToHours(date: string) {
+  const newDate = new Date(date);
+  let hours = newDate.getHours();
+  const minutes = newDate.getMinutes();
+
+  const period = hours >= 12 ? 'PM' : 'AM';
+
+  hours = hours % 12 || 12;
+
+  const hoursFormatted = hours.toString().padStart(2, '0');
+  const minutesFormatted = minutes.toString().padStart(2, '0');
+
+  return `${hoursFormatted}:${minutesFormatted} ${period}`;
+}
+export function convertDateToDay(date: string) {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const newDate = new Date(date);
+  const day = newDate.getDay();
+  const month = newDate.getMonth();
+  const year = newDate.getFullYear();
+
+  return `${months[month]} ${day}, ${year}`;
+}
