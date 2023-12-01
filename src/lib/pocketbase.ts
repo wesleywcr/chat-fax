bcd59b27334966e53cf6bf7587f109535ac9e73c
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-11-30 23:13:30 -0300

    chore: pockebase conection

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -3,4 +3,4 @@ import eventsource from 'react-native-sse';
 // @ts-ignore
 global.EventSource = eventsource;
 
-export const pb = new PocketBase('http://10.0.2.2:8090');
+export const pb = new PocketBase('http://10.0.2.2:8090/');
