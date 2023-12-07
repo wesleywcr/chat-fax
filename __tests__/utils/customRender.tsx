4310109dbfde273cc574b5990df4205bc8b1c4ad
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2023-12-06 22:17:44 -0300

    feat: add custom render

---
+++ __tests__/utils/customRender.tsx
@@ -0,0 +1,21 @@
+import { AuthContextProvider } from '@contexts/AuthContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import type { RenderOptions } from '@testing-library/react-native';
+import { render } from '@testing-library/react-native';
+import type { ReactElement } from 'react';
+
+function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>{children}</AuthContextProvider>
+    </QueryClientProvider>
+  );
+}
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) => render(ui, { wrapper: Providers, ...options });
+
+export * from '@testing-library/react-native';
+export { Providers, customRender as render };
