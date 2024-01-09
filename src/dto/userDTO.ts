188d42ee58804317ce86b3f5876e9dee46ab9111
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2024-01-08 22:52:45 -0300

    feat: add new DTOs messages and user

--- src/dto/userDTO.ts
+++ src/dto/userDTO.ts
@@ -13,15 +13,3 @@ export type UserDTO = {
   phone: string;
   avatar?: string;
 };
-export type IListContacts = {
-  avatar: string;
-  collectionId: string;
-  collectionName: string;
-  created: string;
-  emailVisibility: boolean;
-  id: string;
-  name: string;
-  updated: string;
-  username: string;
-  verified: boolean;
-};
