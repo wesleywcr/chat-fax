a6ab0489bde8cb6ec18ad2f655d577b13c39e99e
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2024-01-11 23:46:18 -0300

    refactor: :recycle: update schema profile

--- src/schemas/profile.ts
+++ src/schemas/profile.ts
@@ -1,20 +1,12 @@
 import { z } from 'zod';
 
-export const profileSchema = z
-  .object({
-    name: z.string({ required_error: 'Nome é um campo obrigatório' }),
-    username: z.string({ required_error: 'Username é um campo obrigatório' }),
-    email: z
-      .string({ required_error: 'E-mail é um campo obrigatório' })
-      .email({ message: 'O campo deve conter um e-mail válido' }),
-    password: z
-      .string({ required_error: 'Senha é um campo obrigatório' })
-      .min(8, { message: 'A senha deve ter no mínimo 8 caracteres' }),
-    passwordConfirm: z
-      .string({ required_error: 'Confirmação de senha é um campo obrigatório' })
-      .min(8, { message: 'A senha deve ter no mínimo 8 caracteres' }),
-  })
-  .refine((data) => data.password === data.passwordConfirm, {
-    path: ['confirmNewPassword'],
-    message: 'As senhas não correspondem',
-  });
+export const profileSchema = z.object({
+  name: z.string({ required_error: 'Name is a required field' }),
+  username: z
+    .string({ required_error: 'Username is a required field' })
+    .min(3, 'Enter at least 3 characters'),
+  phone: z.string({ required_error: 'Telephone is a required field' }),
+  dateOfBirth: z.string({
+    required_error: 'Date of birth is a required field',
+  }),
+});
