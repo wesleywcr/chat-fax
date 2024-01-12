7c2e06078034d2d060293d58c48783b33545010c
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2024-01-11 23:46:57 -0300

    feat: add type IListContacts

---
+++ src/dto/contactsDTO.ts
@@ -0,0 +1,12 @@
+export type IListContacts = {
+  avatar: string;
+  collectionId: string;
+  collectionName: string;
+  created: string;
+  emailVisibility: boolean;
+  id: string;
+  name: string;
+  updated: string;
+  username: string;
+  verified: boolean;
+};
